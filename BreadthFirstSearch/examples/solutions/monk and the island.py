354bc39f891e074f9917a502a83a09e5f49c3d46
kumar2609 <43875508+kumar2609@users.noreply.github.com>
2020-09-27 18:16:31 +0530

    Update monk and the island.py

--- BreadthFirstSearch/examples/solutions/monk and the island.py
+++ BreadthFirstSearch/examples/solutions/monk and the island.py
@@ -1,55 +1,35 @@
-from collections import defaultdict
-from queue import Queue as q
+from collections import defaultdict, deque
+import sys
 
-class node:
-    def __init__(self, value):
-        self.value = value
-        self.count = float('inf')
-        self.p = value
-        self.visited = 'green'
+for _ in range(int(input())):
+    n,m = map(int, sys.stdin.readline().split())
+    graph = defaultdict(list)
+    for i in range(m):
+        v,u = map(int, sys.stdin.readline().split())
+        graph[v].append(u)
+        graph[u].append(v)
 
-class graph:
-    def __init__(self, vertex):
-        self.vertices = vertex
-        self.store = defaultdict(list)
-        self.node={}
-        self.data=q()
 
-    def add_edge(self, u, v):
-        self.store[u].append(v)
-        self.store[v].append(u)
-        if u not in self.node:
-            self.node[u]=node(u)
-        if v not in self.node:
-            self.node[v]=node(v)
-
-    def find(self):
-        self.data.put(self.node[1])
-        self.node[1].count=0
-        while self.data.qsize()!=0:
-            element = self.data.get()
-            
-            for i in self.store[element.value]:
-                if self.node[i].visited == 'green':
-                    self.data.put(self.node[i])
-                    if self.node[i].count>self.node[element.value].count+1:
-                        self.node[i].p = element.value
-                        self.node[i].visited = 'blue'
-                        self.node[i].count=self.node[element.value].count+1
-                elif self.node[i].visited == 'blue':
-                    if self.node[i].count>self.node[element.value].count+1:
-                        self.node[i].p = element.value
-                        self.node[i].visited = 'blue'
-                        self.node[i].count=self.node[element.value].count+1
-            element.visited = 'black'
-        return self.node[self.vertices].count
-
-for i in range(int(input())):
     
-    n,m=map(int,input().split())
-    g=graph(n)
-    for j in range(m):
-        u,v=map(int,input().split())
-        g.add_edge(u,v)
-    print(g.find())
+        
+    q=deque()
+    q.append(1)
+    visited=[False]*(n+1)
+    distance = [float('inf')]*(n+1)
+    distance[0]=0
+    visited[1]=True
+    distance[1]=0
+    
+    while len(q)!=0:
+        element = q.popleft()
+        for i in graph[element]:
+            if not visited[i]:
+                q.append(i)
+                distance[i]=distance[element]+1
+                visited[i] = True
+            if i==n:
+                print(distance[i])
+                break
+        if i==n:
+            break
 
